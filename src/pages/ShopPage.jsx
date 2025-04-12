452fee289537d66a5dc95446c7a4a3937abe87d6
George Kintu <195527580+gkintu@users.noreply.github.com>
2025-04-12 07:16:15 +0200

    feat: add HomePage and ShopPage components with initial content and dependencies

---
+++ src/pages/ShopPage.jsx
@@ -0,0 +1,15 @@
+// src/pages/ShopPage.jsx
+import React from 'react';
+
+function ShopPage() {
+  // We will fetch and display products here later
+  return (
+    <div>
+      <h1>Shop</h1>
+      <p>Check out our products:</p>
+      {/* Product cards will go here */}
+    </div>
+  );
+}
+
+export default ShopPage;
