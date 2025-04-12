4f6a58bbe156024d2fb51c08975ce752184551a2
George Kintu <195527580+gkintu@users.noreply.github.com>
2025-04-12 07:40:56 +0200

    feat: implement Navbar, HomePage, and ShopPage components with basic routing and styles

---
+++ .history/src/components/Navbar_20250412073703.jsx
@@ -0,0 +1,39 @@
+// src/components/Navbar.jsx
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types'; // Import PropTypes
+import './Navbar.css'; // We'll create this CSS file next
+
+function Navbar({ cartItemCount }) { // Receive cartItemCount as a prop
+  return (
+    <nav className="navbar">
+      <Link to="/" className="nav-logo">MyStore</Link>
+      <ul className="nav-links">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/shop">Shop</Link>
+        </li>
+      </ul>
+      <div className="nav-cart">
+        <span>Cart ({cartItemCount})</span>
+        {/* We can add a checkout button/link here later if needed */}
+        {/* <button>Checkout</button> */}
+      </div>
+    </nav>
+  );
+}
+
+// Define prop types for Navbar
+Navbar.propTypes = {
+  cartItemCount: PropTypes.number.isRequired, // Expect cartItemCount to be a required number
+};
+
+// Default props (optional but good practice)
+Navbar.propTypes = {
+  cartItemCount: 0,
+};
+
+
+export default Navbar;
---
+++ .history/src/pages/HomePage_20250412072726.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to Our Store!</h1>
+      <p>Browse our amazing collection of products.</p>
+      {/* Placeholder for images or more content here :) */}
+    </div>
+  );
+}
+
+export default HomePage;
